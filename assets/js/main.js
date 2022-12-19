11fabd2318b5aef487c49eaf6650180fc94581a2
Candil Code <52884009+candil07@users.noreply.github.com>
2022-12-19 13:15:20 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,25 +5,6 @@ var typed = new Typed(".auto-input",{
     loop:true
 })
 
-var elgenaracode = document.querySelector(".genaracode");
-var elgenaracodeCloseButton = elgenaracode.querySelector(".genaracode__close-button");
-var elgenaracodeDismissButton = elgenaracode.querySelector(".genaracode__dismiss-button");
-
-setTimeout(function() {
-	elgenaracode.classList.add("genaracode--open");
-}, 1000);
-
-var closegenaracode = function() {
-	elgenaracode.classList.remove("genaracode--open");
-};
-
-if (elgenaracodeCloseButton) {
-	elgenaracodeCloseButton.addEventListener("click", closegenaracode);
-}
-
-if (elgenaracodeDismissButton) {
-	elgenaracodeDismissButton.addEventListener("click", closegenaracode);
-}
 
 setTimeout(function(){
     if(!document.cookie.includes('open')){
