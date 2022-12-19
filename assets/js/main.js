70faa3f74f1eb46780b0d1034b8215e5864ec342
Candil Code <52884009+candil07@users.noreply.github.com>
2022-12-19 13:08:38 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,4 +60,8 @@ setTimeout(function(){
     };
     
   } )();
-
+var mainDiv = document.getElementById('main-button');
+mainDiv.addEventListener('click', function(){
+  this.children.item(0).classList.toggle('fa-times');
+  this.classList.toggle('open');
+});
